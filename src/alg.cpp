bc34c122c32893c678801e57d043a6f8cbd95609
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-02-20 19:18:34 +0300

    Andrianova IST-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,55 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
+#include <math.h>
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  if (value > 1) {
+        for (int i = 2; i <= sqrt(value); i++)
+            if (value % i == 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+       return true;
+    }
+    else {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+  uint64_t chiclo = 0; 
+  uint64_t i = 0;
+    while (i != n) {
+        chiclo++;
+        if (checkPrime(chiclo))
+            i++;
+    }
+    return chiclo;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+  uint64_t sled_chiclo = 0;
+    sled_chiclo = sled_chiclo + value + 1;
+    while (!(checkPrime(sled_chiclo))) {
+        sled_chiclo++;
+    }
+    return sled_chiclo;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+   uint64_t sum = 0;
+    if (hbound > 2) {
+        for (int i = 2; i < hbound; i++)
+            if (checkPrime(i)) {
+                sum = sum + i;
+            }
+    }
+    return sum;
 }
